ecc871bd1ca69e745e14132d2eba17013d482346
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-09-17 15:40:13 +0300

    Adding Pytest lessons section

---
+++ Pytest_lessons/tests/test_mail_1.py
@@ -0,0 +1,9 @@
+def test_sending_email_1(set_up, some):
+    print("Письмо 1 отправлено")
+
+def test_sending_email_2(set_up, some):
+    print("Письмо 2 отправлено")
+
+
+# def tear_down():
+#     print("Выход из системы")
---
+++ Pytest_lessons/tests/test_mail_2.py
@@ -0,0 +1,5 @@
+def test_sending_email_3(set_up):
+    print("Письмо 1 отправлено")
+
+def test_sending_email_4(set_up):
+    print("Письмо 2 отправлено")
