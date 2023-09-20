73d5c261ba97df81bed1414e8ef8e89157cc6bf6
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-09-20 10:05:13 +0300

    Added new test test_link_about. Updated main_page

--- Main_project/pages/main_page.py
+++ Main_project/pages/main_page.py
@@ -13,11 +13,19 @@ class MainPage(Base):
 
     # Locators
     button_add_to_cart = "add-to-cart-sauce-labs-backpack"
+    sidebar_menu = "react-burger-menu-btn"
+    link_sidebar_menu_about = "about_sidebar_link"
     cart = "shopping_cart_container"
 
     # Getters
     def get_button_add_to_cart(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.button_add_to_cart)))
+
+    def get_sidebar_menu(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.sidebar_menu)))
+
+    def get_link_sidebar_menu_about(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.link_sidebar_menu_about)))
 
     def get_cart(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.cart)))
@@ -27,9 +35,17 @@ class MainPage(Base):
         self.get_button_add_to_cart().click()
         print("Click add to cart button")
 
+    def click_sidebar_menu(self):
+        self.get_sidebar_menu().click()
+        print("Click sidebar menu")
+
+    def click_link_sidebar_menu_about(self):
+        self.get_link_sidebar_menu_about().click()
+        print("Click link 'about' sidebar's menu")
+
     def click_cart(self):
         self.get_cart().click()
-        print("Click and anter the cart")
+        print("Click and enter the cart")
 
     # Methods
     def select_product(self):
@@ -37,3 +53,8 @@ class MainPage(Base):
         self.click_button_add_to_cart()
         self.click_cart()
 
+    def select_sidebar_menu_about(self):
+        self.print_current_url()
+        self.click_sidebar_menu()
+        self.click_link_sidebar_menu_about()
+        self.assert_url("https://saucelabs.com/")
---
+++ Main_project/tests/test_link_about.py
@@ -0,0 +1,35 @@
+import time
+
+from pages.finish_page import FinishPage
+from pages.login_page import LoginPage
+from pages.main_page import MainPage
+from pages.cart_page import CartPage
+from pages.checkout_user_info_page import CheckUserInfoPage
+from pages.payment_page import PaymentPage
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+
+def test_link_product():
+    options = Options()
+    options.add_experimental_option("excludeSwitches", ['enable-logging'])
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+    print(f'=== Start test "{test_link_product.__name__.capitalize()}" ===')
+
+    login = LoginPage(driver)
+    login.authorization()
+
+    mp = MainPage(driver)
+    mp.select_sidebar_menu_about()
+
+
+
+
+    print("=== The test was successful! ===")
