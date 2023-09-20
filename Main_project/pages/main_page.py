951e4bd42bad09e19837a2e9e96fe056f47cfff4
Dmitry Voskoboynik <dmitry.voskoboynik@yandex.ru>
2023-09-20 10:24:53 +0300

    Modified main_page and test_buy_product

--- Main_project/pages/main_page.py
+++ Main_project/pages/main_page.py
@@ -12,14 +12,22 @@ class MainPage(Base):
         self.driver = driver
 
     # Locators
-    button_add_to_cart = "add-to-cart-sauce-labs-backpack"
+    button_add_to_cart_product_1 = "add-to-cart-sauce-labs-backpack"
+    button_add_to_cart_product_2 = "add-to-cart-sauce-labs-bike-light"
+    button_add_to_cart_product_3 = "add-to-cart-sauce-labs-bolt-t-shirt"
     sidebar_menu = "react-burger-menu-btn"
     link_sidebar_menu_about = "about_sidebar_link"
     cart = "shopping_cart_container"
 
     # Getters
-    def get_button_add_to_cart(self):
-        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.button_add_to_cart)))
+    def get_button_add_to_cart_product_1(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.button_add_to_cart_product_1)))
+
+    def get_button_add_to_cart_product_2(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.button_add_to_cart_product_2)))
+
+    def get_button_add_to_cart_product_3(self):
+        return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.button_add_to_cart_product_3)))
 
     def get_sidebar_menu(self):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.sidebar_menu)))
@@ -31,8 +39,16 @@ class MainPage(Base):
         return WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.ID, self.cart)))
 
     # Actions
-    def click_button_add_to_cart(self):
-        self.get_button_add_to_cart().click()
+    def click_button_add_to_cart_product_1(self):
+        self.get_button_add_to_cart_product_1().click()
+        print("Click add to cart button")
+
+    def click_button_add_to_cart_product_2(self):
+        self.get_button_add_to_cart_product_2().click()
+        print("Click add to cart button")
+
+    def click_button_add_to_cart_product_3(self):
+        self.get_button_add_to_cart_product_3().click()
         print("Click add to cart button")
 
     def click_sidebar_menu(self):
@@ -48,10 +64,21 @@ class MainPage(Base):
         print("Click and enter the cart")
 
     # Methods
-    def select_product(self):
+    def select_product_1(self):
         self.print_current_url()
-        self.click_button_add_to_cart()
+        self.click_button_add_to_cart_product_1()
         self.click_cart()
+
+    def select_product_2(self):
+        self.print_current_url()
+        self.click_button_add_to_cart_product_2()
+        self.click_cart()
+
+    def select_product_3(self):
+        self.print_current_url()
+        self.click_button_add_to_cart_product_3()
+        self.click_cart()
+
 
     def select_sidebar_menu_about(self):
         self.print_current_url()
--- Main_project/tests/test_buy_product.py
+++ Main_project/tests/test_buy_product.py
@@ -1,5 +1,7 @@
 import time
 
+import pytest
+
 from pages.finish_page import FinishPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
@@ -16,33 +18,77 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
 
-def test_buy_product():
+@pytest.mark.run(order=2)
+def test_buy_product_1():
     options = Options()
     options.add_experimental_option("excludeSwitches", ['enable-logging'])
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
-    print(f'=== Start test "{test_buy_product.__name__.capitalize()}" ===')
+    print(f'=== Start test "{test_buy_product_1.__name__.capitalize()}" ===')
 
     login = LoginPage(driver)
     login.authorization()
 
     mp = MainPage(driver)
-    mp.select_product()
+    mp.select_product_1()
     print("Enter the cart")
 
     cp = CartPage(driver)
     cp.click_button_checkout()
     print("Go to the checkout information page")
 
-    cuip = CheckUserInfoPage(driver)
-    cuip.input_information()
-    print("Go to the overview page")
+    # cuip = CheckUserInfoPage(driver)
+    # cuip.input_information()
+    # print("Go to the overview page")
+    #
+    # pp = PaymentPage(driver)
+    # pp.payment()
+    #
+    # fp = FinishPage(driver)
+    # fp.finish()
 
-    pp = PaymentPage(driver)
-    pp.payment()
-
-    fp = FinishPage(driver)
-    fp.finish()
+    print(f"=== The '{test_buy_product_1.__name__.capitalize()}' was successful! ===")
 
 
-    print("=== The test was successful! ===")
+@pytest.mark.run(order=1)
+def test_buy_product_2():
+    options = Options()
+    options.add_experimental_option("excludeSwitches", ['enable-logging'])
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+    print(f'=== Start test "{test_buy_product_2.__name__.capitalize()}" ===')
+
+    login = LoginPage(driver)
+    login.authorization()
+
+    mp = MainPage(driver)
+    mp.select_product_2()
+    print("Enter the cart")
+
+    cp = CartPage(driver)
+    cp.click_button_checkout()
+    print("Go to the checkout information page")
+
+    print(f"=== The '{test_buy_product_2.__name__.capitalize()}' was successful! ===")
+
+
+@pytest.mark.run(order=3)
+def test_buy_product_3():
+    options = Options()
+    options.add_experimental_option("excludeSwitches", ['enable-logging'])
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+    print(f'=== Start test "{test_buy_product_3.__name__.capitalize()}" ===')
+
+    login = LoginPage(driver)
+    login.authorization()
+
+    mp = MainPage(driver)
+    mp.select_product_3()
+    print("Enter the cart")
+
+    cp = CartPage(driver)
+    cp.click_button_checkout()
+    print("Go to the checkout information page")
+
+    print(f"=== The '{test_buy_product_3.__name__.capitalize()}' was successful! ===")
